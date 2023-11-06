65d48076ed2f79ede1624d48a99d111fe7fb1aff
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-11-06 20:29:12 +0100

    fixed staticfiles bug

--- btre/settings.py
+++ btre/settings.py
@@ -126,15 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'btre/static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
