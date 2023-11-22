2183df1ad7c8baa4059106d8c570241f985b69ff
Wojtek <121879383+WojciechStancel@users.noreply.github.com>
2023-11-22 23:12:47 +0100

    Update allowed hosts

--- btre/settings.py
+++ btre/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.20.211.102', 'localhost']
 
 
 # Application definition
