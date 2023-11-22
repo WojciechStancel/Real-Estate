f7544d3b16a268c839f41117bff72cc8e61bfc3d
Wojtek <121879383+WojciechStancel@users.noreply.github.com>
2023-11-22 23:18:15 +0100

    changed sec key

--- btre/settings.py
+++ btre/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(str(env_file_path / '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'asda3re3r32tfewtgf436631trdfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
