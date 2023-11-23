b35442bfeea626d85d27d7af6623099e6544e300
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-11-23 16:57:14 +0100

    feature - added email configuration

--- btre/settings.py
+++ btre/settings.py
@@ -152,6 +152,11 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger"
 }
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
