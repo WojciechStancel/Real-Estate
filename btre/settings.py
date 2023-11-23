2a32828aa6a96a1e4444fca105efe20696fa0605
Ubuntu <ubuntu@ip-172-31-25-42.eu-north-1.compute.internal>
2023-11-23 13:30:54 +0000

    initial

--- btre/settings.py
+++ btre/settings.py
@@ -25,10 +25,10 @@ environ.Env.read_env(str(env_file_path / '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asda3re3r32tfewtgf436631trdfv'
+SECRET_KEY = 'asda3a1314./q]][a1.re3r32tfewtgf436631trdfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.20.211.102', 'localhost']
 
